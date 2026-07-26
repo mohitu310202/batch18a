d498dd6534c0fda9a524a7145a6e4074e5bddf7c
Mohit Tiwari <mohitu310202@gmail.com>
2026-07-26 14:28:39 +0530

    changes made in app.py

--- app.py
+++ app.py
@@ -1,82 +1,4 @@
-from flask import Flask, request 
-app = Flask(__name__)
 
-usersList = [
-    {
-        "id": 1,
-        "username": "prashant",
-        "name": "Prashant Dey",
-        "age": 30
-    },
-    {
-        "id": 2,
-        "username": "ashish",
-        "name": "Ashish Kumar",
-        "age": 70
-    },
-    {
-        "id": 3,
-        "username": "sarthak",
-        "name": "Sarthak Nigam",
-        "age": 29
-    }
-]
-
-@app.route('/', methods=['GET'])
-def main():
-    return 'Hello World!'
-
-# @app.route('/user/<string:username>')
-# def user(username):
-#     return f'User: {username}'
-
-
-# @app.route('/user/<string:username>')
-# def user(username):
-#     try:
-#         for user in usersList:
-#             if user["username"].lower() == username.lower():
-#                 return f"Username:{username}, Age: {user["age"]}"
-#             else:
-#                 return {"message: User not found"}
-#     except Exception as e:
-#         print(e)
-#         return {"message:" "something went wrong!!!"}
-                
-
-@app.route('/user/<string:username>')
-def users(username):
-    # Logic to fetch the user and show their age
-    try:
-        for user in usersList:
-            if user['username'].lower() == username.lower():
-                return f"User: {username}, Age: {user['age']}"
-            else:
-                return {"msg": "User not found"}        
-    except Exception as e:
-        print(e)
-        return {"msg": "Something went wrong"}
-
-
-@app.route("/people")
-def people():
-    user = request.args.get("user")
-    age = request.args.get("age")
-    return f"user: {user} | age: {age}"
-
-
-@app.route('/about', methods=['GET'])
-def about():
-    return 'this is about section'
-
-
-@app.route('/contact')
-def contact():
-    return "This is contact"
-
-@app.route('/contact', methods=["GET","POST"])
-def contact():
-    return "This is contact"
 
 
 print("hello mohit")
@@ -84,6 +6,3 @@ print("hello mohit")
 print("hello world")
 
 print("Dey")
-
-if __name__=="__main__":
-      app.run(debug=True, port=5000)
